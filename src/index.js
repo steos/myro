1a2848643b77d1c8402f14fb590dd5708a34d397
Stefan Oestreicher <oestef@gmail.com>
2016-03-03 03:52:16 +0100

    refactor match.

--- src/index.js
+++ src/index.js
@@ -18,30 +18,38 @@ function assemble(routes, prefix = '') {
     }));
 }
 
+function matchPath(path, segment) {
+  const re = new RegExp('^' + segment.replace(placeHolderRegex, '([^/]+)') + '(/.+)?$');
+  return path.match(re);
+}
+
+function findMatch(routes, path) {
+  for (const segment in routes) {
+    if (!routes.hasOwnProperty(segment)) continue
+    const match = matchPath(path, segment)
+    if (match) return [match, segment]
+  }
+  return []
+}
+
 function match(routes, path) {
-    for (const segment in routes) {
-        if (!routes.hasOwnProperty(segment)) continue;
-        const spec = routes[segment];
-        const name = _.isObject(spec) ? spec.$name : spec;
-        const payload = _.isObject(spec) ? _.omit(spec, ['$name', '$routes']) : {};
-        const subRoutes = _.isObject(spec) ? spec.$routes : null;
-        const paramKeys = (segment.match(placeHolderRegex) || []).map(trimLeadingColon);
-        const re = new RegExp('^' + segment.replace(placeHolderRegex, '([^/]+)') + '(/.+)?$');
-        const matched = path.match(re);
-        if (matched) {
-            const remainingPath = _.last(matched);
-            const params = _.zipObject(paramKeys, _.dropRight(_.tail(matched)));
-            if (subRoutes && remainingPath) {
-                const subMatch = match(subRoutes, remainingPath);
-                if (subMatch) {
-                    const [subName, subParams, subRemaining, subPayload] = subMatch
-                    return [[name].concat(subName), _.assign(params, subParams), subRemaining, subPayload];
-                }
-            }
-            return [[name], params, remainingPath, payload];
+    const [matched, segment] = findMatch(routes, path)
+    if (!matched) return null
+    const spec = routes[segment];
+    const name = _.isObject(spec) ? spec.$name : spec;
+    const payload = _.isObject(spec) ? _.omit(spec, ['$name', '$routes']) : {};
+    const subRoutes = _.isObject(spec) ? spec.$routes : null;
+    const paramKeys = (segment.match(placeHolderRegex) || []).map(trimLeadingColon);
+    const remainingPath = _.last(matched);
+    const params = _.zipObject(paramKeys, _.dropRight(_.tail(matched)));
+    if (subRoutes && remainingPath) {
+        const subMatch = match(subRoutes, remainingPath);
+        if (subMatch) {
+            const [subName, subParams, subRemaining, subPayload] = subMatch
+            return [[name].concat(subName), _.assign(params, subParams), subRemaining, subPayload];
         }
     }
-    return null;
+    return [[name], params, remainingPath, payload];
 }
 
 export default function(routes) {
